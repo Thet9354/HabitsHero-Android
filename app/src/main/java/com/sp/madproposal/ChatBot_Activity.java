218f7d9a59840846464a14751317863a20c71cb1
PhoonThetPine <thetpine254@gmail.com>
2024-04-03 16:18:17 +0800

    HabitsHero - V1

--- app/src/main/java/com/sp/madproposal/ChatBot_Activity.java
+++ app/src/main/java/com/sp/madproposal/ChatBot_Activity.java
@@ -97,7 +97,7 @@ public class ChatBot_Activity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(), JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization", "Bearer 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")
+                .header("Authorization", "Your api key here")
                 .post(body)
                 .build();
 
